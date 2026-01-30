8a60ba6520368c93ba9aae85e9f42a75f81588c0
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-30 17:53:15 +0400

    add dto package and BookRequest and BookResponse classes

---
+++ src/main/java/com/example/mongodbexample/dto/BookResponse.java
@@ -0,0 +1,20 @@
+package com.example.mongodbexample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookResponse {
+    private String id;
+    private String title;
+    private String author;
+    private String isbn;
+    private String category;
+    private Integer pageCount;
+    private LocalDateTime publishDate;
+}

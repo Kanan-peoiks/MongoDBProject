f252185706aa549710e1c8cb757cfc43ec3cb8e8
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-30 18:43:20 +0400

    update BookRepository

--- src/main/java/com/example/mongodbexample/repository/BookRepository.java
+++ src/main/java/com/example/mongodbexample/repository/BookRepository.java
@@ -3,5 +3,9 @@ package com.example.mongodbexample.repository;
 import com.example.mongodbexample.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface BookRepository extends MongoRepository<Book,String> {
+    List<Book> findByCategory(String category);
+
 }
